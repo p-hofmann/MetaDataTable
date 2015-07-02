17959c27b4097cba3c7451b7d2d348895b0fe000
p-hofmann <peter.hofmann@hhu.de>
2015-07-02 16:14:21 +0200

    parse_file has now too 'as_list' argument

--- metadatatable.py
+++ metadatatable.py
@@ -64,7 +64,7 @@ class MetadataTable(Compress):
 			assert self._has_unique_columns(list_of_column_names), "Column names must be unique!"
 			return list_of_column_names
 
-	def parse_file(self, file_path, separator=None, column_names=False, comment_line=None):
+	def parse_file(self, file_path, separator=None, column_names=False, comment_line=None, as_list=True):
 		"""
 			Reading comma or tab separated values from a file
 
@@ -76,13 +76,15 @@ class MetadataTable(Compress):
 			@type column_names: bool
 			@param comment_line: character or list of character indication comment lines
 			@type comment_line: str | unicode | list[str|unicode]
+			@param as_list: If true lists are returned, else dicts.
+			@param as_list: bool
 
 			@return: Generator of dictionary representing rows
 			@rtype: generator[ dict[int|long|str|unicode, str|unicode] ]
 			#
 		"""
 		with self.open(file_path) as file_handler:
-			for row in self.parse_stream(file_handler, separator, column_names, comment_line):
+			for row in self.parse_stream(file_handler, separator, column_names, comment_line, as_list):
 				yield row
 
 	def parse_stream(self, stream_input, separator=None, column_names=False, comment_line=None, as_list=True):
